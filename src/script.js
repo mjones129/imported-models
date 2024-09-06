e6ce898bd231be0f4cde5beabff3b4d335e0710e
Matt Jones <me@mattjones.tech>
2024-09-05 20:42:40 -0400

    add draco loader

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import GUI from "lil-gui";
 
 /**
@@ -18,15 +19,15 @@ const scene = new THREE.Scene();
 /**
  * Models
  */
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/");
 const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
 gltfLoader.load(
-  "/models/FlightHelmet/glTF/FlightHelmet.gltf",
+  "/models/Duck/glTF-Draco/Duck.gltf",
   (gltf) => {
-    const children = [...gltf.scene.children];
-    for(const child of children) {
-      scene.add(child)
-    }
-       console.log("Success");
+    scene.add(gltf.scene);
+    console.log("Success");
   },
   () => {
     console.log("Progress");
